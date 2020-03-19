9cec2435cebcf570e505b992c9a1ca929e5495e2
pak11273 <pak11273@gmail.com>
2020-03-19 12:34:52 -0500

    updated masthead responsive size

--- src/App.js
+++ src/App.js
@@ -1,34 +1,62 @@
 import React from "react"
-import logo from "./assets/images/SELF.svg"
+import portrait from "./assets/images/SELF.svg"
+import git from "./assets/images/github-logo.png"
+import linkedIn from "./assets/images/pngwave.png"
+import stacked from "./assets/images/stacked-logo.png"
 import "./assets/scss/index.scss"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img
-          width="400px"
-          height="400px"
-          src={logo}
-          className="App-logo"
-          alt="logo"
-        />
-        <h1>Isaac Pak</h1>
-        <p>
-          I am a self taught web developer with more than 3+ years of experience
-          coupled with a digital marketing background. I have professionally
-          worked in the Graphic Design Industry for 5+ years. I have a variety
-          of skills but I'm looking to continue my growth in web development.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <div className="nav row center-xs">
+          <div className="main__menu col-xs-10">
+            <div className="main__menu__item main__menu__item--padding scale-in-center main__menu__item--hover delay-3">
+              <img width="30px" height="30px" src={git} alt="git icon" />
+            </div>
+            <div className="main__menu__item main__menu__item--padding scale-in-center main__menu__item--hover delay-3">
+              <img
+                width="30px"
+                height="30px"
+                src={linkedIn}
+                alt="linkedIn icon"
+              />
+            </div>
+            <div className="main__menu__item main__menu__item--padding scale-in-center main__menu__item--hover delay-3">
+              <img width="37px" height="37px" src={stacked} alt="SO icon" />
+            </div>
+            <div className="main__menu__item--padding scale-in-center main__menu__item--hover delay-4">
+              <p>cv</p>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div className="wrapper">
+        <div className="row center-xs ">
+          <section className="masthead">
+            <img src={portrait} className="vibrate-1" alt="self portrait" />
+            <div className="puff-in-center-rider masthead__rider">
+              <h5>fullStack</h5>
+            </div>
+            <div>
+              <h2>Isaac Pak</h2>
+              <h4>Web Developer</h4>
+            </div>
+            <div>
+              <p>built with</p>
+              <p>
+                <span className="masthead__text--hightlight">
+                  create-react-app
+                </span>
+                <span> & </span>
+                <span className="masthead__text--hightlight">sass</span>
+              </p>
+              <p>See the source code</p>
+            </div>
+          </section>
+        </div>
+        <section className="row center-xs"></section>
+      </div>
     </div>
   )
 }
